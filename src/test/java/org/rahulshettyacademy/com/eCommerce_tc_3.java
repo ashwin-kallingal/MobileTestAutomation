9f46934f1e65c4e6849b37b45441a25772e9dbcc
ashwin <ashwin.kallingal@gmail.com>
2024-07-18 19:00:57 +0530

    My Commit

---
+++ src/test/java/org/rahulshettyacademy/com/eCommerce_tc_3.java
@@ -0,0 +1,33 @@
+package org.rahulshettyacademy.com;
+
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.rahulshettyacademy.TestUtils.AndroidBaseTest;
+import org.rahulshettyacademy.pageObjects.android.CartPage;
+import org.rahulshettyacademy.pageObjects.android.ProductPage;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.google.common.collect.ImmutableMap;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
+
+public class eCommerce_tc_3 extends AndroidBaseTest {
+
+	@Test(groups = {"Smoke"})
+	public void alogin() throws InterruptedException {
+
+		fp.setNameField("Shwetha GN");
+		fp.selectGender("Female");
+		fp.selectCountry("Australia");
+		fp.clickLetsShopButton();
+		}
+}
